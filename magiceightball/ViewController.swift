4a53f2833c964e1a183baf373ef75a2f7abb29b9
Jane Cui <jane.anqi.cui@gmail.com>
2023-04-04 17:11:21 -0400

    Initial Commit

---
+++ magiceightball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magiceightball
+//
+//  Created by Jane Cui on 2023-04-04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
